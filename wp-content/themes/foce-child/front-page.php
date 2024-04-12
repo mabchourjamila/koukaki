235d6e6fa382aa5d7ae4a403070d2a6ee4f8ae63
Jamila BOUMEHDI <mabchourjamila87@gmail.com>
2024-04-12 16:24:45 +0200

    ajout swiper dans personnages (slider)

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -27,28 +27,34 @@ get_header();
             ?>
             <article id="characters">
                 <div class="main-character">
-                    <h3>Les personnages</h3>
-                    <?php
-                    $main_character = $characters_query->posts[0];
-                    echo '<figure>';
-                    echo get_the_post_thumbnail( $main_character->ID, 'full' );
-                    echo '<figcaption>'. $main_character->post_title . '</figcaption>';
-                    echo '</figure>';
-                    $characters_query->next_post();
-                    ?>
+                    <h3>Les personnages</h3>                    
                 </div>
-                <div class="other-characters">
-                    <?php
-                    while ( $characters_query->have_posts() ) {
-                        $characters_query->the_post();
-                        echo '<figure>';
-                        echo get_the_post_thumbnail( get_the_ID(), 'full' );
-                        echo '<figcaption>';
-                        the_title();
-                        echo'</figcaption>';
-                        echo '</figure>';
-                    }
-                    ?>
+                <!-- Slider main container -->
+                <div class="swiper">
+                    <!-- Additional required wrapper -->
+                    <div class="swiper-wrapper">
+                        <!-- Slides -->
+                        <?php
+                        while ( $characters_query->have_posts() ) {
+                            $characters_query->the_post();
+                            echo '<div class="swiper-slide"><figure>';
+                            echo get_the_post_thumbnail( get_the_ID(), 'full' );
+                            echo '<figcaption>';
+                            the_title();
+                            echo'</figcaption>';
+                            echo '</figure></div>';
+                        }
+                        ?>                    
+                    </div>
+                    <!-- If we need pagination -->
+                    <!--<div class="swiper-pagination"></div>-->
+
+                    <!-- If we need navigation buttons -->
+                    <!--<div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>-->
+
+                    <!-- If we need scrollbar -->
+                    <!--<div class="swiper-scrollbar"></div>-->
                 </div>
             </article>
             <article id="place">
